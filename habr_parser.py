5889fd2a9cbfb7cc97196931e1a78cc3e4c966ee
AleksandrSushchenko <039pro@gmail.com>
2022-12-07 16:06:30 +0200

    Проверка работы запроса IP посчитали количество статей

--- habr_parser.py
+++ habr_parser.py
@@ -12,11 +12,25 @@ def get_page(url):
 
     return requests.get(url, headers=headers.generate())
 
+def parser_articl(artical_tag):
+    time = artical_tag.find('time')['datetime']
+    title = artical_tag.find('h2').find('span').text
+    link = artical_tag.find('a')['href']
+    link = f'{HABR}{link}'
+
+    return {
+        'time':time,
+        'title':title,
+        'link':link
+    }
+
 def main():
     main_html = get_page(HABR_ARTICLES).text
     soup = BeautifulSoup(main_html, features='lxml')
     articles = soup.find_all('article')
-    print(len(articles))
+    for arcticl in articles:
+        parsed = parser_articl(arcticl)
+        print(parsed)
 
 if __name__ == '__main__':
     main()
